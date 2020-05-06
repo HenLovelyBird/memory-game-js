02a6a5cbd22430205aafdb68ceac38aa7c28641f
henlovelybird <voxylophone@gmail.com>
2020-05-06 13:06:44 +0200

    initial setup

---
+++ app.js
@@ -0,0 +1,67 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //card options:
+    const cardArray = [
+        {
+            name: 'cheeseburger',
+            img: 'img/cheeseburger.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'img/cheeseburger.png'
+        },
+        {
+            name: 'fries',
+            img: 'img/fries.png'
+        },
+        {
+            name: 'fries',
+            img: 'img/fries.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'img/hotdog.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'img/hotdog.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'img/ice-cream.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'img/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'img/milkshake.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'img/milkshake.png'
+        },
+        {
+            name: 'pizza',
+            img: 'img/pizza.png'
+        },
+        {
+            name: 'pizza',
+            img: 'img/pizza.png'
+        }
+    ]
+
+    const grid = document.querySelector(.grid)
+
+    //create the board
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img')
+            card.setAttribute('src', 'images/blank.png')
+            card.setAttribute('data-id', i)
+            // card.addEventListener('click', flipCard)
+
+        }
+    }
+})
